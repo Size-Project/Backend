17b8f43a571b01e6fbf6274153be05a37fa286ac
robolovo <dlwnstj21@naver.com>
2021-11-24 15:43:19 +0900

    feat: 유저 Service 및 Controller 구현

--- src/main/java/com/dailyhome/back/user/presentation/UserController.java
+++ src/main/java/com/dailyhome/back/user/presentation/UserController.java
@@ -1,12 +1,33 @@
 package com.dailyhome.back.user.presentation;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.dailyhome.back.user.presentation.dto.request.UserSignUpRequest;
+import com.dailyhome.back.user.presentation.dto.response.UserResponse;
+import com.dailyhome.back.user.service.UserService;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(value = "*")
 @RequestMapping("/api/users")
 @RestController
 public class UserController {
 
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<?> registerUser(@RequestBody UserSignUpRequest userSignUpRequest) {
+        userService.save(userSignUpRequest);
+        return ResponseEntity.ok("success");
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> findUserById(@PathVariable("id") Long id) {
+        UserResponse userResponse = userService.findById(id);
+        return ResponseEntity.ok(userResponse);
+    }
 }
