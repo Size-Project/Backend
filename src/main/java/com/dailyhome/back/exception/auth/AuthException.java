51f96e7eaca69f44ae265a87b0af72bda9e61485
robolovo <dlwnstj21@naver.com>
2021-11-24 15:47:55 +0900

    feat: Exception 클래스 추가

--- src/main/java/com/dailyhome/back/exception/auth/AuthException.java
+++ src/main/java/com/dailyhome/back/exception/auth/AuthException.java
@@ -1,2 +1,11 @@
-package com.dailyhome.back.exception.auth;public class AuthException {
+package com.dailyhome.back.exception.auth;
+
+import com.dailyhome.back.exception.ApplicationException;
+import org.springframework.http.HttpStatus;
+
+public abstract class AuthException extends ApplicationException {
+
+    protected AuthException(String errorCode, HttpStatus httpStatus, String message) {
+        super(errorCode, httpStatus, message);
+    }
 }
--- src/main/java/com/dailyhome/back/exception/user/UserException.java
+++ src/main/java/com/dailyhome/back/exception/user/UserException.java
@@ -1,2 +1,10 @@
-package com.dailyhome.back.exception.user;public class UserException {
+package com.dailyhome.back.exception.user;
+
+import com.dailyhome.back.exception.ApplicationException;
+import org.springframework.http.HttpStatus;
+
+public abstract class UserException extends ApplicationException {
+    protected UserException(String errorCode, HttpStatus httpStatus, String message) {
+        super(errorCode, httpStatus, message);
+    }
 }
